9789ac81b6a5acb3f30c07d2d16e48e9f1893ef3
Alireza Khalili <alireza.khalili@check24.de>
2023-04-09 12:42:59 +0200

    add TLS

---
+++ make-certs.sh
@@ -0,0 +1,6 @@
+#!/bin/bash -xe
+mkdir -p certs
+rm certs/*
+openssl req -new -nodes -x509 -out certs/server.pem -keyout certs/server.key -days 3650 -subj "/C=DE/ST=NRW/L=Earth/O=Random Company/OU=IT/CN=www.random.com/emailAddress=alireza.khalili@check24.de"
+openssl req -new -nodes -x509 -out certs/client.pem -keyout certs/client.key -days 3650 -subj "/C=DE/ST=NRW/L=Earth/O=Random Company/OU=IT/CN=www.random.com/emailAddress=alireza.khalili@check24.de"
+
