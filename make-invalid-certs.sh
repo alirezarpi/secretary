63f9393d77d6841bea488fb0405c48aa2605566f
Alireza Khalili <alirezarpi@pc-74-142.customer.ask4.lan>
2023-04-09 13:24:58 +0200

    remove unwanted files

--- make-invalid-certs.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash -xe
-mkdir -p certs-invalid
-rm certs-invalid/*
-#openssl req -new -nodes -x509 -out certs-invalid/server.pem -keyout certs-invalid/server.key -days 3650 -subj "/C=DE/ST=NRW/L=Earth/O=Random Company/OU=IT/CN=www.random.com/emailAddress=alireza.khalili@pipi.de"
-openssl req -new -nodes -x509 -out certs-invalid/client.pem -keyout certs-invalid/client.key -days 3650 -subj "/C=DE/ST=NRW/L=Earth/O=Random Company/OU=IT/CN=www.random.com/emailAddress=alireza.khalili@pipi.de"
-
