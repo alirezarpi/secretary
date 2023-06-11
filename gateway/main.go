9dc93bad93f1f6d41f53b00011005796fd38cfc7
Alirezarpi <alireza.kh077@yahoo.com>
2023-06-11 19:43:39 +0200

    adding storage

--- gateway/main.go
+++ gateway/main.go
@@ -8,29 +8,13 @@ import (
 	"secretary/alpha/api"
 )
 
-type requestData struct {
-	Inputs []map[string]interface{} `json:"data"`
-}
-
-func home(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/" {
-		api.Responser(w, r, false, 404, map[string]interface{}{
-		"message": "path not found",
-		})
-		return
-	}
-	api.Responser(w, r, true, 200, map[string]interface{}{
-		"message": "Secretary is here ^^",
-	})
-}
 
 func main() {
-	
 	var listenAddr = flag.String("listenaddr", "0.0.0.0:6080", "secretary server address")
 	flag.Parse()
 	var handler = http.NewServeMux()
 
-	handler.HandleFunc("/", home)
+	handler.HandleFunc("/", api.Home)
 	handler.HandleFunc("/hz", api.HealthCheck)
 
 	var server = &http.Server{
