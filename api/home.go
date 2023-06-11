2426f3378b7ab0f45c8615574036c685d7ae1f80
Alirezarpi <alireza.kh077@yahoo.com>
2023-06-11 19:43:47 +0200

    adding storage

---
+++ api/home.go
@@ -0,0 +1,17 @@
+package api
+
+import (
+	"net/http"
+)
+
+func Home(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		Responser(w, r, false, 404, map[string]interface{}{
+		"message": "path not found",
+		})
+		return
+	}
+	Responser(w, r, true, 200, map[string]interface{}{
+		"message": "Secretary is here ^^",
+	})
+}
