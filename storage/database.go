72beea596b19e8f2fdec1bce8eee3cbc97415ea1
Alirezarpi <alireza.kh077@yahoo.com>
2023-06-25 18:14:18 +0200

    more refactor

--- storage/database.go
+++ storage/database.go
@@ -2,6 +2,7 @@ package storage
 
 import (
 	"log"
+	"fmt"
 	"database/sql"
 
 	_ "github.com/mattn/go-sqlite3"
@@ -18,21 +19,28 @@ func OpenDatabase() (*sql.DB) {
 
 func DatabaseInit() bool {
 	db := OpenDatabase()
-
-	query := `CREATE TABLE IF NOT EXISTS asks_for (
-	  uuid TEXT NOT NULL PRIMARY KEY,
-	  what TEXT NOT NULL,
-	  created_time DATETIME NOT NULL,
-	  modified_time DATETIME NOT NULL,
-	  reason TEXT NOT NULL,
-	  status TEXT NOT NULL
-	);`
-
-	_, err := db.Exec(query)
-	if err != nil {
-		log.Fatal("Error in DatabaseInit: ", err)
-		return false
+	
+	initTables := []string{ // put columns too
+		"asks_for", 
+		"users",
 	}
 
+	for _, table := range initTables {
+		query := fmt.Sprintf(`CREATE TABLE IF NOT EXISTS %s (
+		  uuid TEXT NOT NULL PRIMARY KEY,
+		  what TEXT NOT NULL,
+		  created_time DATETIME NOT NULL,
+		  modified_time DATETIME NOT NULL,
+		  reason TEXT NOT NULL,
+		  status TEXT NOT NULL
+		);`, table)
+
+		_, err := db.Exec(query)
+		if err != nil {
+			log.Fatal("Error in DatabaseInit table ", table, " : ", err)
+			return false
+		}
+	}
+	
 	return true
 }
