85fe1120a189445ac52450fc58093d879fd675d9
Alireza Khalili <alireza.kh077@yahoo.com>
2023-07-26 22:38:52 +0200

    refactor asks for internal, not working yet

--- internal/ask.go
+++ internal/ask.go
@@ -9,26 +9,60 @@ import (
 	"secretary/alpha/utils"
 )
 
-func CreateAsk(what string, reason string) bool {
+type AsksFor struct {
+	UUID         string
+	What		 string
+	Reason		 string
+	Status		 string
+	Requester	 string
+	Reviewer	 string
+	CreatedTime  string
+	ModifiedTime string
+}
+
+func (af *AsksFor) CreateAsksFor(what string, reason string) error {
 	uuid := utils.UUID()
 	createdTime := utils.CurrentTime()
 	status := constants.ASK_PENDING
 	requester := "placeholder"
 	reviewer := "placeholder"
 
-	query := fmt.Sprintf(`INSERT INTO asks_for (uuid, what, created_time, modified_time, reason, status)
-		VALUES ('%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s')`, uuid, what, createdTime, createdTime, reason, status, requester, reviewer)
+	query := `INSERT INTO asks_for (uuid, what, created_time, modified_time, reason, status, requester, reviewer)
+		VALUES ('%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s')`
+	_, err := storage.DatabaseExec(query, uuid, what, createdTime, createdTime, reason, status, requester, reviewer)
 
-	log.Printf("Asking query: %s", query)
-
-	_, err := storage.DatabaseExec(query)
 	if err != nil {
 		return false
 	}
 	return true
 }
 
-func GetAsk(uuid ...string) []map[string]interface{} {
+func (af *AsksFor) GetAsksFor(uuid string) *AsksFor {
+	query := fmt.Sprintf(`SELECT * FROM asks_for WHERE uuid='%s'`, uuid[0])
+
+	rows, err := storage.DatabaseQuery(query)
+	if err != nil {
+		log.Fatal("Error in GetAsk: ", err)
+		return []map[string]interface{}{}
+	}
+	defer rows.Close()
+
+	columns, err := rows.Columns()
+	if err != nil {
+		log.Fatal("Error in GetAsk: ", err)
+		return []map[string]interface{}{}
+	}
+
+	results, err := utils.HandleTableToJSON(columns, rows)
+	if err != nil {
+		log.Fatal("Error in GetAsk: ", err)
+		return []map[string]interface{}{}
+	}
+
+	return results
+}
+
+func (af *AsksFor) GetAsksFors() *AsksFor {
 	var query string
 	if len(uuid) > 0 {
 		query = fmt.Sprintf(`SELECT * FROM asks_for WHERE uuid='%s'`, uuid[0])
