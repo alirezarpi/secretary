e9e2a0eff8024c700675ded0163ebb4a7388cccd
Alirezarpi <alireza.kh077@yahoo.com>
2023-08-01 22:53:18 +0200

    what is wrong with you

---
+++ api/self.go
@@ -0,0 +1,27 @@
+package api
+
+import (
+	"net/http"
+
+	"secretary/alpha/internal"
+)
+
+func SelfAPI(w http.ResponseWriter, r *http.Request) {
+	if Middleware(w, r) {
+		if (r.Method != "GET") {
+			Responser(w, r, false, 405, map[string]interface{}{
+				"error": "method not allowed",
+			})
+		}
+
+		user := &internal.User{}
+		_, user = isAuthenticated(r)
+		Responser(w, r, true, 200, map[string]interface{}{
+			"user_data": user,
+		})
+	} else {
+		Responser(w, r, false, 401, map[string]interface{}{
+			"error": "Unauthorized",
+		})
+	}
+}
