1c048f68fed92eb813df6997a2c21136767a96b4
Alirezarpi <alireza.kh077@yahoo.com>
2023-08-13 19:02:16 +0200

    add debug log

--- utils/log.go
+++ utils/log.go
@@ -12,13 +12,20 @@ func Logger(logLevel string, message string) {
 		file = "unknown"
 		line = 0
 	}
+	// FIXME limit the show logLevel if debug is not considered, then don't
 	switch logLevel {
+	case "debug":
+		log.Printf("[DEBUG] %s:%d - %s\n", file, line, message)
+		return
 	case "info":
 		log.Printf("[INFO] %s:%d - %s\n", file, line, message)
+		return
 	case "warn":
 		log.Printf("[WARN] %s:%d - %s\n", file, line, message)
+		return
 	case "err":
 		log.Printf("[ERROR] %s:%d - %s\n", file, line, message)
+		return
 	case "fatal":
 		log.Fatalf("[FATAL] %s:%d - %s\n", file, line, message)
 	}
